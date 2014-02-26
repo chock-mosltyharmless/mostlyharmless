9ea952bbd65f870906bfd85864ce2fe86494ae14
chock.mostlyharmless@gmail.com <chock.mostlyharmless@gmail.com@5fa19ecb-53a5-36b8-5b22-f21608b8f541>
2014-02-26 19:21:01 +0000

    Perspective projection on GPU

--- the_move/screensaver/project/i4k_OGL/src/TextureManager.cpp
+++ the_move/screensaver/project/i4k_OGL/src/TextureManager.cpp
@@ -394,12 +394,12 @@ void TextureManager::drawQuad(float startX, float startY, float endX, float endY
 	glBegin(GL_QUADS);
 	glTexCoord2f(0.0f, 1.0f);
 	// bl
-	glVertex3f(c*startX - s*endY + midX, s*startX + c*endY + midY, 0.5);
+	glVertex3f(c*startX - s*endY + midX, s*startX + c*endY + midY, 0.5f);
 	glTexCoord2f(1.0f, 1.0f);
-	glVertex3f(c*endX - s*endY + midX, s*endX + c*endY + midY, 0.5);
+	glVertex3f(c*endX - s*endY + midX, s*endX + c*endY + midY, 0.5f);
 	glTexCoord2f(1.0f, 0.0f);
-	glVertex3f(c*endX - s*startY + midX, s*endX + c*startY + midY, 0.5);
+	glVertex3f(c*endX - s*startY + midX, s*endX + c*startY + midY, 0.5f);
 	glTexCoord2f(0.0, 0.0f);
-	glVertex3f(c*startX - s*startY + midX, s*startX + c*startY + midY, 0.5);
+	glVertex3f(c*startX - s*startY + midX, s*startX + c*startY + midY, 0.5f);
 	glEnd();
 }
