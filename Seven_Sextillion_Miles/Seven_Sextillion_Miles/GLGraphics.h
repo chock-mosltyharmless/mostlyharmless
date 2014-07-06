8fdb2a4560895f7e1da130a0bef1a827bdbcf3ea
chock.mostlyharmless@gmail.com <chock.mostlyharmless@gmail.com@5fa19ecb-53a5-36b8-5b22-f21608b8f541>
2014-07-06 18:46:44 +0000

    Basic sprite rendering in GLGraphics

--- Seven_Sextillion_Miles/Seven_Sextillion_Miles/GLGraphics.h
+++ Seven_Sextillion_Miles/Seven_Sextillion_Miles/GLGraphics.h
@@ -3,6 +3,7 @@
 #define WIN32_LEAN_AND_MEAN             // Selten verwendete Teile der Windows-Header nicht einbinden.
 #include <windows.h>
 #include <GL/GL.h>
+#include "GLTextures.h"
 
 // Global strings (better in a resource file?)
 #define GL_WINDOW_TITLE "Seven Sextillion Miles"
@@ -15,6 +16,7 @@
 #define GL_SET_PIXEL_FORMAT_FAILED -5
 #define GL_CREATE_CONTEXT_FAILED -6
 #define GL_MAKE_CURRENT_FAILED -7
+#define GL_LOAD_TEXTURES_FAILED -8
 
 class GLGraphics
 {
@@ -32,12 +34,37 @@ public:
 	// Resize the window. May return some negative error
 	int resize(int width, int height);
 
-	// Swap double buffer screens
+	// Stop rendering (quads) and swap double buffer screens
 	// May return negative error values
 	int swap(void);
 
 	// Clears screen with black. May return negative error values
+	// This also initializes rendering (glBegin(GL_QUADS));
 	int clear(void);
+
+	// Get the texture ID from a named texture
+	// That might either be a .png or any of the special textures
+	// Returns 0 if successful, -1 otherwise
+	// The error string must hold space for at least MAX_ERROR_LENGTH
+	// characters.
+	int setTexture(const char *name, char *errorString) {
+		unsigned int texID;
+		if (textures.getTextureID(name, &texID, errorString) == 0) {
+			glBindTexture(GL_TEXTURE_2D, texID);
+			return 0;
+		}
+		else return -1;
+	}
+
+	// xOrigin is -1 for left, 0 for center, 1 for right
+	// yOrigin is -1 for bottom, 0 for center, 1 for top
+	// For a negative origin, the position direction is inverted
+	// ?Pos, width and height are relative to the shorter length of the viewport.
+	void drawSprite(int xOrigin, int yOrigin, float xPos, float yPos,
+					float width, float height, float rotation);
+
+	// Display an error and wait for user to acknowledge it
+	void handleError(const char *errorString);
 
 private:
 	bool wglExtensionSupported(const char *extension_name);
@@ -45,5 +72,12 @@ private:
 	HDC hDC;		// Private GDI Device Context
 	HGLRC hRC;		// Permanent Rendering Context
 	HWND hWnd;		// Holds Our Window Handle
+
+	// Size of the screen viewport
+	int viewportWidth;
+	int viewportHeight;
+
+	// Texture manager
+	GLTextures textures;
 };
 
