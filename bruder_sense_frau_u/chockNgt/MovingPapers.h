47354316e5f73f9eeba61b37583fbae600d22ab6
chock.mostlyharmless <chock.mostlyharmless@gmail.com>
2015-02-26 19:10:09 +0000

    rendertarget

--- bruder_sense_frau_u/chockNgt/MovingPapers.h
+++ bruder_sense_frau_u/chockNgt/MovingPapers.h
@@ -1,13 +1,12 @@
 #pragma once
 
 #include "TextureManager.h"
-#include "ScreenBorders.h"
 
 #define NUM_PAPER_TEXTURES 9
 #define PAPER_PERIOD 8.0
 #define PAPER_MOVEMENT_TIME 0.2
 // Delay for a paper left to a right one to draw
-#define PAPER_MOVEMENT_DELTA 0.25
+#define PAPER_MOVEMENT_DELTA 0.0
 
 class MovingPapers
 {
@@ -15,7 +14,7 @@ public:
 	MovingPapers();
 	virtual ~MovingPapers(void);
 
-	void draw(float time, HWND mainWnd, TextureManager *texManag, ScreenBorders *borders, bool drawVideo);
+	void draw(float time, HWND mainWnd, TextureManager *texManag, bool drawVideo);
 
 private:
 	void drawQuad(float left, float bottom, float right, float top, float leftU, float rightU);
