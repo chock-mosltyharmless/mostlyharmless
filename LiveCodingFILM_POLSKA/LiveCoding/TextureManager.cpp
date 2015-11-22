04f5d86029413384e694951c9977eb6a2ee646fb
Joachim Hofer <chock.mostlyharmless@gmail.com>
2015-11-22 15:47:12 +0100

    better default shader

--- LiveCodingFILM_POLSKA/LiveCoding/TextureManager.cpp
+++ LiveCodingFILM_POLSKA/LiveCoding/TextureManager.cpp
@@ -112,8 +112,8 @@ int TextureManager::loadTGA(const char *filename, char *errorString)
 	glBindTexture(GL_TEXTURE_2D, textureID[numTextures]);
 	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);	// Linear Filtering
 	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);	// Linear Filtering
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	//glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	//glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 #if 0 // Use simple mipmap generation	
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA,
 				 textureWidth[numTextures], textureHeight[numTextures],
