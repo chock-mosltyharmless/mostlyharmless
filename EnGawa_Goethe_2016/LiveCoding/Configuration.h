8c35be5d0e7184c237bcf8158d80330ebb176473
Joachim Hofer <Joachim Hofer>
2016-08-10 19:56:24 +0200

    Basic Engawa @ Goethe render engine (Hermaniak livecoding derivative)

---
+++ EnGawa_Goethe_2016/LiveCoding/Configuration.h
@@ -0,0 +1,23 @@
+#ifndef CONFIGURATION_H
+#define CONFIGURATION_H
+#pragma once
+
+// This header contains all the configuration macros that are
+// used to set the resolution and so on.
+#define XRES 1600
+#define YRES (XRES*9/16)
+
+// The resolution of the background stuff
+#define X_OFFSCREEN XRES
+#define Y_OFFSCREEN YRES
+#define X_HIGHLIGHT 1200
+#define Y_HIGHLIGHT (X_HIGHLIGHT * 3 / 4)
+
+// The maximum number of chars in an error message.
+// Make error fields one larger!
+#define MAX_ERROR_LENGTH (32*1024)
+
+// Play back music during coding
+//#define MUSIC
+
+#endif
---
+++ EnGawa_Goethe_2016/LiveCoding/TextureManager.h
@@ -0,0 +1,106 @@
+/**
+ * The texture manager holds all the textures that the system supports.
+ * Additionally to the textures in the ./textures directory, it also
+ * holds some special textures. Here is a list of textures:
+ * TODO: Shall the texture manager handle the render target stuff or not????
+ * - 'noise2D': Core 2D noise texture. In the future there might be more
+ * - 'noise3D': Core 3D noise texture.
+ * - 'renderTarget': Core render target at render target resolution
+ * - 'smallTarget': Smaller render target for hypnoglow and the like
+ */
+
+#pragma once
+
+#define TM_DIRECTORY "textures/"
+#define TM_SHADER_WILDCARD "*.tga"
+#define TM_MAX_NUM_TEXTURES 64
+#define TM_MAX_FILENAME_LENGTH 1024
+
+#define TM_OFFSCREEN_NAME "renderTarget"
+#define TM_HIGHLIGHT_NAME "smallTarget"
+#define TM_NOISE_NAME "noise2D"
+#define TM_NOISE3D_NAME "noise3D"
+#define TM_DEPTH_SENSOR_NAME "depthSensor"
+
+// The noise texture dimension
+#define TM_NOISE_TEXTURE_SIZE 256
+#define TM_FILTER_KERNEL_SIZE 32
+
+// Name of the 32x32x32 noise texture
+#define TM_3DNOISE_TEXTURE_SIZE 16 // try smaller?
+
+// Kinect Header files
+#include <Kinect.h>
+
+struct TGAHeader
+{
+	unsigned char identSize;
+	unsigned char colourmapType;
+	unsigned char imageType;
+
+	// This is a stupid hack to fool the compiler.
+	// I do not know what happens if I compile it
+	// under release conditions.
+	unsigned char colourmapStart1;
+	unsigned char colourmapStart2;
+	unsigned char colourmapLength1;
+	unsigned char colourmapLength2;
+	unsigned char colourmapBits;
+
+	short xStart;
+	short yStart;
+	short width;
+	short height;
+	unsigned char bits;
+	unsigned char descriptor;
+};
+
+class TextureManager
+{
+public:
+	TextureManager(void);
+	~TextureManager(void);
+
+public: // functions
+	// Load all textures from the ./textures directory.
+	// Returns 0 if successful, -1 otherwise
+	// The error string must hold space for at least SM_MAX_ERROR_LENGTH
+	// characters. It contains errors from compilation/loading
+	int init(char *errorString);
+
+	// Get the texture ID from a named texture
+	// That might either be a .tga or any of the special textures
+	int getTextureID(const char *name, GLuint *id, char *errorString);
+
+private: // functions
+	void releaseAll(void);
+	int createRenderTargetTexture(char *errorString, int width, int height,
+								  const char *name);
+	int loadTGA(const char *filename, char *errorString);
+	float frand(void);
+	void normalizeKernel(float *kernel, int kernelLength);
+	void normalizeTexture(float *texture, int textureSize);
+	void makeIntTexture(void);
+	void generateNoiseTexture(void);
+	int createNoiseTexture(char *errorString, const char *name);
+	int createNoiseTexture3D(char *errorString, const char *name);
+	int initializeDefaultSensor(char *errorString);  // Initializes Kinect
+	int CreateSensorTexture(char *errorString, const char *name);
+	int UpdateSensorTexture(char *error_string, GLuint texture_index);
+	
+private: // data
+	int numTextures;
+	char textureName[TM_MAX_NUM_TEXTURES][TM_MAX_FILENAME_LENGTH+1];
+	GLuint textureID[TM_MAX_NUM_TEXTURES];
+	int textureWidth[TM_MAX_NUM_TEXTURES];
+	int textureHeight[TM_MAX_NUM_TEXTURES];
+
+	// Kinect data
+	IKinectSensor *kinect_sensor_;
+	IDepthFrameReader *depth_frame_reader_;
+	INT64 last_frame_time_;  // Last time that a kinect frame was aquired
+	float *cpu_depth_sensor_buffer_;
+	float *smoothed_depth_sensor_buffer_[2];
+	int next_smoothed_depth_sensor_buffer_;
+};
+
