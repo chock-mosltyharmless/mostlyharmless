f80d8ef41002f8fd997c599a44f709491d9ba8fc
Joachim Hofer <Joachim Hofer>
2016-11-06 17:40:21 +0100

    First version of animate over video

---
+++ animate_over_video/chockNgt/Configuration.h
@@ -0,0 +1,28 @@
+#ifndef CONFIGURATION_H
+#define CONFIGURATION_H
+#pragma once
+
+#define PIF 3.1415926f
+
+// This header contains all the configuration macros that are
+// used to set the resolution and so on.
+#define XRES 1024
+#define YRES 768
+
+// The resolution of the background stuff
+//#define X_OFFSCREEN 512
+//#define Y_OFFSCREEN 256
+extern int X_OFFSCREEN;
+extern int Y_OFFSCREEN;
+#define X_HIGHLIGHT 128
+#define Y_HIGHLIGHT 64
+
+// The maximum number of chars in an error message.
+// Make error fields one larger!
+#define MAX_ERROR_LENGTH (32*1024)
+
+#define NO_SCENE_BRIGHTNESS 0.3f
+
+//#define FULLSCREEN
+
+#endif
---
+++ animate_over_video/chockNgt/stdafx.h
@@ -0,0 +1,23 @@
+// stdafx.h : Includedatei für Standardsystem-Includedateien
+// oder häufig verwendete projektspezifische Includedateien,
+// die nur in unregelmäßigen Abständen geändert werden.
+//
+
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN             // Selten verwendete Teile der Windows-Header nicht einbinden.
+// Windows-Headerdateien:
+#include <windows.h>
+
+// C RunTime-Headerdateien
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+#include <stdio.h>
+#include <math.h>
+#include <Mmsystem.h>
+
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include "glext.h"
