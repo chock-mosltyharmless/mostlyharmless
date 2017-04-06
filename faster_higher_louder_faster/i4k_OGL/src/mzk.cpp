5abc3a92d95c04b886df2193b56c21db765f0d33
Joachim Hofer <Joachim Hofer>
2017-04-06 21:01:23 +0200

    First version of faster_higher_louder_faster (sound beat only)

---
+++ faster_higher_louder_faster/i4k_OGL/src/mzk.cpp
@@ -0,0 +1,273 @@
+//--------------------------------------------------------------------------//
+// iq / rgba  .  tiny codes  .  2008                                        //
+//--------------------------------------------------------------------------//
+
+#include <math.h>
+#include "mzk.h"
+#include <emmintrin.h>
+
+#ifdef WRITE_MUSIC
+#include <stdio.h>
+#endif
+
+#ifndef PI
+#define PI 3.1415f
+#endif
+#define LOG_2_E 1.44269f
+#define fScaler ((float)((double)2*PI / (double)MZK_RATE))
+
+// Size of a buffer with random numbers
+#define RANDOM_BUFFER_SIZE 65536
+
+static float randomBuffer[RANDOM_BUFFER_SIZE];
+static float lowNoise[RANDOM_BUFFER_SIZE];
+// The same as random Buffer, but contains exp(4*(randomBuffer-1))
+static float expRandomBuffer[RANDOM_BUFFER_SIZE];
+
+static unsigned long seed;
+
+// create random value between -65534 and 65534?
+#pragma code_seg(".rand")
+int rand()
+{
+	unsigned long a = 214013;
+	unsigned long c = 2531011;
+	unsigned long m = 4294967296-1;
+	seed = (seed * a + c) % m;
+	return (seed >> 8) % 65535;
+}
+
+// TODO: Check implementation from somewhere else. Esp. %65535? Numeric recipies.
+#pragma code_seg(".frand")
+float frand()
+{
+	return (float)(rand()) * (1.0f/65536.0f);
+}
+
+inline int ftoi_fast(float f)
+{
+    return _mm_cvtt_ss2si(_mm_load_ss(&f));     // SSE1 instructions for float->int
+}
+
+#pragma code_seg(".exp2jo")
+static double exp2jo(double f)
+{
+	__asm fld f;
+	__asm fld st;
+	__asm frndint;
+	__asm fsub st(1), st;
+	__asm fxch;
+	__asm f2xm1;
+	__asm fld1;
+	__asm fadd;
+	__asm fscale;
+	__asm fstp st(1);
+	__asm fstp f;
+
+	return f;
+}
+
+#pragma code_seg(".mzkPlayBlock")
+static float floatOutput[MZK_BLOCK_SIZE][2];
+void mzkPlayBlock(short *blockBuffer)
+{
+	// clear audio block
+	for (int sample = 0; sample < MZK_BLOCK_SIZE * 2; sample++)
+	{
+		// Stereo! --> 2 values
+		floatOutput[0][sample] = 0;
+	}
+
+    static int startSampleID = 0;
+    static float fdrum_position = 0;
+    int sampleID = startSampleID;
+    static float last_loudness = 1.0f;
+    float loudness = last_loudness;
+    static int last_drum_position = 0;
+    static int skipped_drum_position = 0;
+
+    for (int sample = 0; sample < MZK_BLOCK_SIZE; sample++) {
+        const int kMinDrumStep = 64;
+        const int kDrumStep = 512 * kMinDrumStep;
+
+        float speedup = 0.5f;
+        if (speedup > 0.5f) speedup = 0.5f;
+        speedup += (float)((last_drum_position + skipped_drum_position) % (kDrumStep*4)) / (kDrumStep*4) / 2;
+        float multiplier = 1.0f / speedup;
+        float total_speed = (float)(sampleID) / (1<<22);
+        if (total_speed > 0.5f) total_speed = 0.5f;
+        //total_speed = 0.5f;
+        fdrum_position += speedup * total_speed;
+        if (fdrum_position > kDrumStep) {
+            fdrum_position -= kDrumStep;
+            skipped_drum_position += kDrumStep;
+        }
+
+        int drum_position = (int)(fdrum_position);
+        // Count ones before the first zero
+        // I may do this more intelligent by divinding by 2 given some stuffs
+        float drum_loudness = 0.0f;
+        if (drum_position != last_drum_position) {
+            if (!(drum_position & (2 * kMinDrumStep - 1))) {
+                if (drum_position & (2 * kMinDrumStep)) {
+                    if (drum_position & (4 * kMinDrumStep)) {
+                        if (drum_position & (8 * kMinDrumStep)) {
+                            if (drum_position & (16 * kMinDrumStep)) {
+                                if (drum_position & (32 * kMinDrumStep)) {
+                                    if (drum_position & (64 * kMinDrumStep)) {
+                                        if (drum_position & (128 * kMinDrumStep)) {
+                                            if (drum_position & (256 * kMinDrumStep)) {
+                                                drum_loudness = 1.0f;
+                                            } else {
+                                                drum_loudness = (0.5f) * multiplier;
+                                            }
+                                        } else {
+                                            drum_loudness = (0.25f) * multiplier;
+                                        }
+                                    } else {
+                                        drum_loudness = (0.125f) * multiplier;
+                                    }
+                                } else {
+                                    drum_loudness = (0.0625f) * multiplier;
+                                }
+                            } else {
+                                drum_loudness = (0.03125f) * multiplier;
+                            }
+                        } else {
+                            drum_loudness = (0.015625f) * multiplier;
+                        }
+                    } else {
+                        drum_loudness = (0.0078125f) * multiplier;
+                    }
+                } else {
+                    drum_loudness = (0.00390625f) * multiplier;
+                }
+            } else {
+                drum_loudness = 0.0f;
+            }
+        }
+        last_drum_position = drum_position;
+        if (drum_loudness > loudness) {
+            loudness = drum_loudness;
+        }
+        //loudness += drum_loudness;
+
+        const float kAmplitudeReduction = 1.0f - 0.001f * (total_speed + 0.05f);
+        floatOutput[sample][0] = (total_speed + 0.02f) * 32.0f * loudness * lowNoise[sampleID % RANDOM_BUFFER_SIZE];
+        //floatOutput[sample][0] = loudness;
+        floatOutput[sample][1] = (total_speed + 0.02f) * 32.0f * loudness * lowNoise[(sampleID+800) % RANDOM_BUFFER_SIZE];;
+        //floatOutput[0][sample] = loudness * sinf(sampleID * 0.01f);
+        loudness *= kAmplitudeReduction;
+        sampleID++;
+    }
+
+    startSampleID = sampleID;
+    last_loudness = loudness;
+
+	// Copy to int output
+	for (int sample = 0; sample < MZK_BLOCK_SIZE * 2; sample++)
+	{
+#if 0
+		int val = (int)floatOutput[0][sample];
+		if (val > 32767) val = 32767;
+		if (val < -32767) val = -32767;
+		blockBuffer[sample] = val;
+#else
+		float val = floatOutput[0][sample];
+		if (val > 1.0f) val = 1.0f;
+		if (val < -1.0f) val = -1.0f;
+		val = (float)sin(val) * 32512.0f;
+		blockBuffer[sample] = ftoi_fast(val);
+#endif
+	}
+
+#ifdef WRITE_MUSIC
+    FILE *fid = fopen("music.raw", "ab");
+    fwrite(blockBuffer, sizeof(*blockBuffer), MZK_BLOCK_SIZE * 2, fid);
+    fclose(fid);
+#endif
+}
+
+// put here your synth
+#pragma code_seg(".mzkInit")
+void mzk_init()
+{
+	// Create data tables
+	// make frequency (Hz) table
+#if 0
+	float k = 1.059463094359f;	// 12th root of 2
+	float a = 6.875f;	// a
+	a *= k;	// b
+	a *= k;	// bb
+	a *= k;	// c, frequency of midi note 0
+	for (int i = 0; i < kNumFrequencies; i++)	// 128 midi notes
+	{
+		freqtab[i] = (float)a;
+		a *= k;
+	}
+#endif
+
+	// Make table with random number
+	seed = 1;
+	for (int i = 0; i < RANDOM_BUFFER_SIZE; i++)
+	{
+		randomBuffer[i] = frand();
+		expRandomBuffer[i] = (float)exp2jo(LOG_2_E * 4.0f * (randomBuffer[i] - 1));
+		lowNoise[i] = 4.0f * (randomBuffer[i] - 0.5f);
+	}
+
+#if 0
+	// Set the oscillator phases to zero
+	for (int inst = 0; inst < NUM_INSTRUMENTS; inst++)
+	{
+		for (int i = 0; i < NUM_OVERTONES; i++)
+		{
+			for (int j = 0; j < NUM_Stereo_VOICES; j++)
+			{
+				fPhase[inst][i][j] = 0.f;
+			}
+		}
+
+		// Don't play instrument
+		iADSR[inst] = 3;
+	}
+#endif
+
+	// Ring-low-pass-filtering of lowPass
+	// Use a one-pole
+	float oldVal = 0.0f;
+	for (int j = 0; j < 4; j++)
+	{
+		for (int i = 0; i < RANDOM_BUFFER_SIZE; i++)
+		{
+			lowNoise[i] = 1.0f / 8.0f * lowNoise[i] + (1.0f - 1.0f / 8.0f) * oldVal;
+			oldVal = lowNoise[i];
+		}
+	}
+
+	// Clear reverberation buffer
+#if 0
+	for (int inst = 0; inst < NUM_INSTRUMENTS; inst++)
+	{
+		for (int i = 0; i < NUM_Stereo_VOICES; i++)
+		{
+			for (int j = 0; j < MAX_DELAY_LENGTH; j++)
+			{
+				reverbBuffer[inst][i][j] = 0;
+			}
+			reverbBufferLength[inst][i] = 1;
+		}
+	}
+#endif
+
+
+#if 0
+	// create some test output
+	for (int block = 0; block < MZK_DURATION; block++)
+	{
+		// 2x due to stereo
+		short *blockBuffer = buffer + 2 * block*MZK_BLOCK_SIZE;
+		mzkPlayBlock(blockBuffer);
+	}
+#endif
+}
---
+++ faster_higher_louder_faster/i4k_OGL/src/mzk.h
@@ -0,0 +1,24 @@
+//--------------------------------------------------------------------------//
+// iq / rgba  .  tiny codes  .  2008                                        //
+//--------------------------------------------------------------------------//
+
+#ifndef _MZK_H_
+#define _MZK_H_
+
+#define MZK_BLOCK_SIZE  4096
+#define AUDIO_BUFFER_SIZE MZK_BLOCK_SIZE
+#define MZK_DURATION    1292
+#define MZK_RATE        44100
+#define MZK_NUMCHANNELS 2
+
+#define MZK_NUMSAMPLES  (MZK_DURATION*MZK_BLOCK_SIZE)
+#define MZK_NUMSAMPLESC (MZK_NUMSAMPLES*MZK_NUMCHANNELS)
+
+// Debug: Dump music to file
+#define WRITE_MUSIC
+
+void mzk_init();
+void mzkPlayBlock(short *blockBuffer);
+
+
+#endif
