3f09c3438e0d0422e0619dc1579508ad13413551
Joachim Hofer <Joachim Hofer>
2017-05-15 21:00:43 +0200

    basic bugfix of art critic fail features

--- computer_contemplating_art/FailCritic/LiveCoding.cpp
+++ computer_contemplating_art/FailCritic/LiveCoding.cpp
@@ -123,6 +123,128 @@ float blob = 0.;
 
 static LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
+typedef struct {
+    double r;       // a fraction between 0 and 1
+    double g;       // a fraction between 0 and 1
+    double b;       // a fraction between 0 and 1
+} rgb;
+
+typedef struct {
+    double h;       // angle in degrees
+    double s;       // a fraction between 0 and 1
+    double v;       // a fraction between 0 and 1
+} hsv;
+
+static hsv   rgb2hsv(rgb in);
+static rgb   hsv2rgb(hsv in);
+
+hsv rgb2hsv(rgb in)
+{
+    hsv         out;
+    double      min, max, delta;
+
+    min = in.r < in.g ? in.r : in.g;
+    min = min  < in.b ? min  : in.b;
+
+    max = in.r > in.g ? in.r : in.g;
+    max = max  > in.b ? max  : in.b;
+
+    out.v = max;                                // v
+    delta = max - min;
+    if (delta < 0.00001)
+    {
+        out.s = 0;
+        out.h = 0; // undefined, maybe nan?
+        return out;
+    }
+    if( max > 0.0 ) { // NOTE: if Max is == 0, this divide would cause a crash
+        out.s = (delta / max);                  // s
+    } else {
+        // if max is 0, then r = g = b = 0              
+        // s = 0, v is undefined
+        out.s = 0.0;
+        out.h = NAN;                            // its now undefined
+        return out;
+    }
+    if( in.r >= max )                           // > is bogus, just keeps compilor happy
+        out.h = ( in.g - in.b ) / delta;        // between yellow & magenta
+    else
+        if( in.g >= max )
+            out.h = 2.0 + ( in.b - in.r ) / delta;  // between cyan & yellow
+        else
+            out.h = 4.0 + ( in.r - in.g ) / delta;  // between magenta & cyan
+
+    out.h *= 60.0;                              // degrees
+
+    if( out.h < 0.0 )
+        out.h += 360.0;
+
+    return out;
+}
+
+
+rgb hsv2rgb(hsv in)
+{
+    double      hh, p, q, t, ff;
+    long        i;
+    rgb         out;
+
+    // Convert h to range 0..360 (Note this is slow for large values...)
+    while (in.h < 0) in.h += 360.0;
+    while (in.h >= 360.0) in.h -= 360.0;
+
+    if(in.s <= 0.0) {       // < is bogus, just shuts up warnings
+        out.r = in.v;
+        out.g = in.v;
+        out.b = in.v;
+        return out;
+    }
+    hh = in.h;
+    if(hh >= 360.0) hh = 0.0;
+    hh /= 60.0;
+    i = (long)hh;
+    ff = hh - i;
+    p = in.v * (1.0 - in.s);
+    q = in.v * (1.0 - (in.s * ff));
+    t = in.v * (1.0 - (in.s * (1.0 - ff)));
+
+    switch(i) {
+    case 0:
+        out.r = in.v;
+        out.g = t;
+        out.b = p;
+        break;
+    case 1:
+        out.r = q;
+        out.g = in.v;
+        out.b = p;
+        break;
+    case 2:
+        out.r = p;
+        out.g = in.v;
+        out.b = t;
+        break;
+
+    case 3:
+        out.r = p;
+        out.g = q;
+        out.b = in.v;
+        break;
+    case 4:
+        out.r = t;
+        out.g = p;
+        out.b = in.v;
+        break;
+    case 5:
+    default:
+        out.r = in.v;
+        out.g = p;
+        out.b = q;
+        break;
+    }
+    return out;     
+}
+
 static void window_end(WININFO *info)
 {
     if (info->hRC)
@@ -549,10 +671,11 @@ void intro_do(long t, long delta_time)
     glBindTexture(GL_TEXTURE_2D, textureID);
 
     const int num_passes = 4;
-
+    static unsigned long seed = 1;
+    seed += 1351356;
     // TODO: Make the first 1 or 2 passes to the smaller backbuffer
     for (int pass = 0; pass < num_passes; pass++) {
-        // Set small
+        srand(seed);
 
         // In the first pass, use highlight
         if (pass < num_passes - 3) {
@@ -576,6 +699,20 @@ void intro_do(long t, long delta_time)
 
         // Draw one iteration of the IFS
         float transformation[2][3];
+        for (int i = 0; i < 12; i++) {
+            float size = 0.7f + 0.3f * sinf(rand() * 0.001f);
+            transformation[0][0] = 0.7f * sinf(rand() * 0.001f) * size;
+            transformation[0][1] = 0.7f * sinf(rand() * 0.001f) * size;
+            transformation[0][2] = 0.3f * sinf(rand() * 0.001f) * sinf(rand() * 0.001f);
+            transformation[1][0] = 0.7f * sinf(rand() * 0.001f) * size;
+            transformation[1][1] = 0.7f * sinf(rand() * 0.001f) * size;
+            transformation[1][2] = 0.3f * sinf(rand() * 0.001f) * sinf(rand() * 0.001f);
+            hsv incolor = {(float)(rand() % 1000), 0.3f, 0.6f};
+            rgb outcolor = hsv2rgb(incolor);
+            DrawQuad(transformation, (float)outcolor.r, (float)outcolor.g, (float)outcolor.b, 1.0f);
+        }
+#if 0
+        // Old buggy code
         for (int i = 0; i < 20; i++) {
             transformation[0][0] = 0.7f * sinf(rand() * 0.001f) * sinf(rand() * 0.001f);
             transformation[0][1] = 0.7f * sinf(rand() * 0.001f) * sinf(rand() * 0.001f);
@@ -588,6 +725,7 @@ void intro_do(long t, long delta_time)
             float blue = 0.5f + 0.5f * sinf(rand() * 0.001f);
             DrawQuad(transformation, red, green, blue, 1.0f);
         }
+#endif
 
         // Copy backbuffer to texture
         if (pass < num_passes - 3) {
